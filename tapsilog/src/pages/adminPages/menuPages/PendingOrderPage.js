f16004bf8153c9051a2bef578abcc308a97b929c
VexIvannnnnnn <sumangvex@gmail.com>
2025-04-04 21:26:28 +0800

    done for admin

--- tapsilog/src/pages/adminPages/menuPages/PendingOrderPage.js
+++ tapsilog/src/pages/adminPages/menuPages/PendingOrderPage.js
@@ -18,6 +18,20 @@ export default function PendingOrderPage() {
         }
     };
 
+    const updateStatus = async (orderID) => {
+        try {
+            const response = await axios.put(`http://localhost:5000/order/updateOrder/${orderID}`, {
+                Status: "served",
+            });
+            if (response.status === 200) {
+                fetchOrders();
+            }
+        } catch (e) {
+            console.error("Error updating order status:", e);
+            alert("Failed to update order status.");
+        }
+    }
+    console.log("Pending orders: ",pendingOrders);
     return (
         <div>
             <h1>Pending Order Page</h1>
@@ -27,15 +41,19 @@ export default function PendingOrderPage() {
                 <ul>
                     {pendingOrders.map((order) => (
                         <li key={order._id}>
-                            <h3>Order ID: {order._id}</h3>
+                            <h3>Order ID: {order._id}<br/> {order.CustomerNumber ? `Customer Number: ${order.CustomerNumber}` : ""}
+                            {order.TableID ? `Table Number: ${order.TableID}` : ""}</h3>
+                            
                             <ul>
                                 {order.Content.map((item, index) => (
                                     <li key={index}>
                                         {item.Food_Name} - Quantity: {item.quantity}
+                                        {console.log(item)}
                                     </li>
                                 ))}
                             </ul>
                             <p>Total: {order.Total}</p>
+                            <button onClick={() => updateStatus(order._id)}> Served </button>
                         </li>
                     ))}
                 </ul>
